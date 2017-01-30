634a22c90e3a178986d547c4d819214e73bccdf1
crucialHawg <clem.lepisiser@gmail.com>
2017-01-30 22:30:04 +0100

    des truc

---
+++ js/app.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+  var heyyyyyyyyy = 0;
+  console.log('heyyyyyyyyy :', heyyyyyyyyy)
+  heyyyyyyyyy++
+  window.setTimeout(function(){
+    console.log('heyyyyyyyyy :', heyyyyyyyyy)
+  },555)
+}
